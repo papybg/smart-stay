50415a4d2668049665b5153806edb251a537717b
Temporary User <temp@example.com>
2026-01-28 22:35:37 +0200

    nowo oprawjane na ime na model

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.post('/api/chat', async (req, res) => {
     `;
 
     // --- ТУК СА ТВОИТЕ МОДЕЛИ ---
-    const modelsToTry = ["gemini-3.0-flash-preview", "gemini-flash-latest"];
+    const modelsToTry = ["gemini-3.0-pro-preview", "gemini-flash-latest", "gemini-3-flash-preview"];
     let finalReply = "Ико има техническо затруднение.";
 
     for (const modelName of modelsToTry) {
